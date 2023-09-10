e10bcda0b03351235ed35d6db8c8cb4a1ba157cd
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2023-09-10 11:50:00 +0200

    first

---
+++ main/player.ts
@@ -0,0 +1,22 @@
+import { RpgPlayer, type RpgPlayerHooks, Control, Components } from '@rpgjs/server'
+
+const player: RpgPlayerHooks = {
+    onConnected(player: RpgPlayer) {
+        player.name = 'Player'
+        player.setComponentsTop(Components.text('{name}'))
+    },
+    onInput(player: RpgPlayer, { input }) {
+        if (input == Control.Back) {
+            player.callMainMenu()
+        }
+    },
+    async onJoinMap(player: RpgPlayer) {
+        if (player.getVariable('AFTER_INTRO')) {
+            return
+        }
+        await player.showText('Welcome to my Portfolio World!')
+        player.setVariable('AFTER_INTRO', true)
+    }
+}
+
+export default player
